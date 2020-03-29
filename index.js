e2834328f0149099ad103f96b816720ca0604c6d
Katie <Katie@Sandstone.local>
2020-03-29 10:08:57 +0900

    add playground

--- index.js
+++ index.js
@@ -62,7 +62,12 @@ const resolvers = {
   }
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true
+});
 
 server
   .listen({
